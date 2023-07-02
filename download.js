95b4ae7a622393ad8c0122cab44aff49142fd590
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-07-03 00:50:45 +0300

    Update download.js
    testing smth

--- download.js
+++ download.js
@@ -1,1 +1,4 @@
-function download(){window.location.assign('https://github.com/sn0wgit/tlvk_reload/releases/download/v2.8.beta/TLVK_Reload-2.8_Beta.zip');}
+function download(){
+  alert("🤨");
+  window.location.assign('https://github.com/sn0wgit/tlvk_reload/releases/download/v2.8.beta/TLVK_Reload-2.8_Beta.zip');
+}
