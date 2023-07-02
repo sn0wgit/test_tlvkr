d7e3b8379f4566881ab7ef289fedb3b622dc85f7
sn0wgit <87296601+sn0wgit@users.noreply.github.com>
2023-07-03 01:10:38 +0300

    Update download.js

--- download.js
+++ download.js
@@ -1,4 +1,9 @@
-function download(){
+const button = document.getElementById("download");
+button.onclick = function () {
   alert("🤨");
   window.location.assign('https://github.com/sn0wgit/tlvk_reload/releases/download/v2.8.beta/TLVK_Reload-2.8_Beta.zip');
 }
+/*function download(){
+  alert("🤨");
+  window.location.assign('https://github.com/sn0wgit/tlvk_reload/releases/download/v2.8.beta/TLVK_Reload-2.8_Beta.zip');
+}*/
